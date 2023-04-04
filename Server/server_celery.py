062208957a2ef7aeecf36a8c0f641152075478d9
RedEvilBK <anh.nguyenminhbku@hcmut.edu.vn>
2023-04-04 19:36:09 +0700

    Update server
    oidoioi

--- Server/server_celery.py
+++ Server/server_celery.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify, request
-import base64
 import os
-import time
+import base64
+from celery import Celery
+from time import sleep
 
 app = Flask(__name__)
+
+app.config['CELERY_BROKER_URL'] = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+app.config['CELERY_RESULT_BACKEND'] = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
+# celery.conf.update(app.config)
+
 
 # Set allowed image extensions
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -18,16 +25,20 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def process_image(img_bytes):
+@celery.task
+def process_image(img_str):
     """Process uploaded image with AI model and return distance of object in frame"""
+    # Decode the string back to bytes using the base64 module
+    img_bytes = base64.b64decode(img_str.encode('utf-8'))
+    
     # TODO: add AI model code to process image and return distance of object
-    distance = 10  # Placeholder value for demonstration purposes only
+    distance = 20  # Placeholder value for demonstration purposes only
+    sleep(4)
     return distance
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # print(request.files)
     # Check CSRF token
     if request.headers.get('X-CSRF-Token') != app.secret_key:
         return jsonify({'error': 'Invalid CSRF token.'}), 403
@@ -36,16 +47,22 @@ def upload():
     if 'image' not in request.files:
         return jsonify({'error': 'No image provided.'}), 400
 
+
     image_file = request.files['image']
     # Check if file has allowed extension
     if not allowed_file(image_file.filename):
         return jsonify({'error': 'File must be in PNG, JPG, or JPEG format.'}), 400
 
-    # Read file contents from memory, encode as base64, and return as JSON response
+    # Read image bytes and process image in the background with AI model to get distance of object in frame
     img_bytes = image_file.read()
-    distance = process_image(img_bytes)
+    # Encode the img_bytes argument as a string (JSON serializable object)
+    img_str = base64.b64encode(img_bytes).decode('utf-8')
+    task = process_image.delay(img_str)
+    distance = task.get()
+
+    # Return distance of object in frame
     return jsonify({'distance': distance}), 200
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
